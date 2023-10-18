f08e95084194a52025c703930ca7aaa0e3dd8e21
subwina <72843419+subwina@users.noreply.github.com>
2023-10-18 17:39:59 +0800

    Create ReadThis.cpp

---
+++ ReadThis.cpp
@@ -0,0 +1,51 @@
+Anti Semitic Initiative
+
+
+
+I deeply feel heartbroken about the conflict between Palestine and Israel
+
+I deeply dislike the remarks made by Israeli officials
+
+Reflection on the Jewish Past
+
+I have found that the root cause of Jews' lack of acceptance by the people of the world
+
+Lies in the teachings of Judaism
+
+Its teachings indicate that Jews are chosen by God
+
+But the rest of us (people of any country or ethnic group, non Jewish)
+
+They are all inferior and not even worthy of being human beings, only worthy of being slaves to Jews
+
+This belief is extremely anti human
+
+It fills Jews with arrogance and contempt when facing others
+
+But the Jews are not wrong, what is wrong is this doctrine
+
+It makes Jews believe that only Jews are human, and that others are no different from animals
+
+So Israeli officials can confidently claim that the Palestinians are humanoid creatures without any burden in their hearts
+
+So I propose that people all over the world unite
+
+Abolish Judaism
+
+however
+
+Please do not target Jewish ethnicity
+
+The bad thing is Judaism
+
+Rather than Jewish ethnicity
+
+
+
+If we don't take action
+
+When one day in the future
+
+A country ruled or supported by right-wing Jews becomes stronger
+
+We all become slaves
